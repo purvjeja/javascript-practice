28c0ea87480e0c8714ea5a67059106f86468dcc1
purvjeja <jejapurv@gmail.com>
2021-06-11 16:25:54 +0530

    React Problems

--- REACT JS/problemone/src/App.js
+++ REACT JS/problemone/src/App.js
@@ -1,126 +1,90 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-let numberforproblemthree = 5;
 
 function App() {
   return (
     <div id="App" className="App">
       <header className="App-header">
-        <br></br>
-        <div id = "menu">
-        <button onClick={() => problem1() }>PROBLEM 1</button>
-        <button onClick={() => problem2() }>PROBLEM 2</button>
-        <button onClick={() => problem3() }>PROBLEM 3</button>
+        
+        <div id="menu" className="problem">
+        <Problem1 /> <hr />
+        <Problem2 /> <hr />
+        <Problem3 />
         </div>
-        <div id="problems"></div>
+        <div id="problems" className="problem">
+        </div>
       </header>
     </div>
   );
 }
+// function Names(props){
+//   return <h1>Hello, {props.name} {props.surname}</h1>; 
+// }
 
-function problem1(){
-  let problemdivision = document.getElementById('problems');
-  problemdivision.innerHTML = " ";
-  let divMain =  document.createElement('div');
-  divMain.className = "main";
-  let problemstatementdivision = document.createElement('div');
-  problemstatementdivision.className = "problemstatement";
-  problemstatementdivision.innerHTML = "<h3>Problem Statement 1 : To remove image on Button click.</h3>";
+function Problem1(){
+  var status = useState('none');  
+  const imagetoggling = (status) => { document.getElementById('image').style.display = status; }
+
+  return (
+      <div>
+      <h1>Problem 1</h1>
+      <h3>To toggle image</h3>
+      <img id="image" src={logo} alt="React"/> <br />
+      <button  onClick={() => {status = ((status === 'none')?'block':'none');  imagetoggling(status);}}> Toggle </button>
+      </div>
+  )      
+}
+
+function Problem2(){
+  const [multiplicationTable,setMultiplicationTableValue] = useState([]);
   
-  let button = document.createElement('button');
-  button.innerText = "Click Me";
-  button.addEventListener('click',() => { removeImage() });
-  
-  let image = document.createElement('img');
-  image.src = logo;
-  image.className = "App-logo";
-  image.id="image";
-  
-  divMain.append(problemstatementdivision);
-  divMain.append(image);
-  divMain.append(button);
-  problemdivision.append(divMain);
-}
-
-function removeImage(){
-  let imageStatus = document.getElementById('image');
-  imageStatus.style.display = (imageStatus.style.display === 'none')? 'block': 'none';
-  let imageStatusText = (imageStatus.style.display === 'none')? 'Removed':'Added';
-  setTimeout(function(){ alert(`Image ${imageStatusText} Successfully. `) },100);
-}
-
-function problem2(){
-  let problemdivision = document.getElementById('problems');
-  problemdivision.innerHTML = " ";
-  let divMain =  document.createElement('div');
-  divMain.className = "main";
-  let resultDiv = document.createElement('div');
-  let problemstatementdivision = document.createElement('div');
-  problemstatementdivision.className = "problemstatement";
-  problemstatementdivision.innerHTML = "<h3>Problem Statement 2 : To Create Table of any number.</h3>";
-  let inputvalue = parseInt(prompt("Enter a number for table?"));
-  if(!(isNaN(inputvalue))) {
-      let array = calculateTable(inputvalue);
-      for(let i of array){
-      resultDiv.innerHTML +=  i + "<br>";
-      }
-      let button = document.createElement('button');
-      button.innerHTML = " TRY AGAIN!";
-      button.addEventListener('click',() => { problem2() });
-
-      divMain.append(problemstatementdivision); 
-      divMain.append(resultDiv);
-      divMain.append(button);
-      problemdivision.append(divMain);
+  function calculateTable(){
+    const arrayoftable = [];
+    const inputvalue = parseInt(prompt("Enter a number for table?"));
+    if((isNaN(inputvalue))) {
+      alert("NOT A NUMBER!"); 
+      setMultiplicationTableValue([]);
+      return;
+    }
+    for(let i=0 ; i<=10 ; i++)  arrayoftable.push(inputvalue + " * " + i + " = " + i * inputvalue);
+    setMultiplicationTableValue(arrayoftable);
   }
-  else{
-    alert("NOT A NUMBER!");
+  return(
+    <div>
+      <h1>Problem 2</h1>
+      <h3>To Create Multiplication Table of any number.</h3>
+      { multiplicationTable.map((result) => <p>{result}</p>)}
+      <button onClick={calculateTable}> {(multiplicationTable.length === 0) ?'Calculate':'Calculate Again'} </button>
+    </div>
+  )
+}
+
+function Problem3(){
+  const [currentNumber,setCurrentNumber] = useState(5);
+  const incrementNumber = () =>{
+    if(currentNumber + 1 > 10) {
+      alert("Can't Increase");
+      return;
+    }
+    setCurrentNumber(currentNumber + 1);
+  } 
+  const decrementNumber = () => {
+    if(currentNumber - 1 < 0){ 
+      alert("Can't Decrease");
+      return;
+    }
+    setCurrentNumber(currentNumber - 1);
   }
+  return(
+    <div>
+      <h1>Problem 2</h1>
+      <h3>Increment or Decrement a Number</h3>
+      <button onClick={incrementNumber}> +  </button>
+      <h1>{currentNumber}</h1>
+      <button onClick={decrementNumber}> - </button>
+    </div>
+  )
 }
 
-function calculateTable(number){
-  let arrayoftable = [];
-  for(let i=0 ; i<=10 ; i++) arrayoftable.push(number + " * " + i + " = " + i*number);
-  return arrayoftable; 
-}
-
-function problem3(){
-  let problemdivision = document.getElementById('problems');
-  problemdivision.innerHTML = " ";
-  let divMain =  document.createElement('div');
-  divMain.className = "main";
-  let problemstatementdivision = document.createElement('div');
-  problemstatementdivision.className = "problemstatement";
-  problemstatementdivision.innerHTML = "<h3>Problem Statement 3 : To have two buttons : '+' & '-', By clicking should reflect to a default number. </h3>";
-  
-  let resultDiv = document.createElement('div');
-  resultDiv.id = 'result';
-  resultDiv.innerHTML = '<h1> Number : ' + numberforproblemthree + '</h1>';
-
-  let buttonAdd = document.createElement('button');
-  buttonAdd.innerText = "+";
-  buttonAdd.setAttribute('style',"width:40px; height:40px;");
-  buttonAdd.addEventListener('click',() => {changeNumber('+')});
-  let buttonSub = document.createElement('button');
-  buttonSub.setAttribute('style',"width:40px; height:40px; margin-left:20px;");
-  buttonSub.innerText = "-";
-  buttonSub.addEventListener('click', () => {changeNumber('-')});
-
-  divMain.append(problemstatementdivision);
-  divMain.append(resultDiv);
-  divMain.append(buttonAdd);
-  divMain.append(buttonSub);
-  problemdivision.append(divMain);
-}
-function changeNumber(id){
-  if(id === "+"){
-    if(numberforproblemthree > 9) alert("Can't Increase more than 10");
-    else numberforproblemthree++;
-  }
-  else{
-    if(numberforproblemthree < 1) alert("Can't decrease more than 0");
-    else numberforproblemthree--;
-  }
-  document.getElementById('result').innerHTML = '<h1> Number : ' + numberforproblemthree + '</h1>';
-}
 export default App;
