6ca0fba8f9fd29c93bc30b27ffecf9e634c3f089
purvjeja <jejapurv@gmail.com>
2021-09-30 00:31:21 +0530

    L

--- Typescript/todolist/src/App.tsx
+++ Typescript/todolist/src/App.tsx
@@ -1,3 +1,4 @@
+import { red } from '@material-ui/core/colors';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 interface ListBox {
@@ -12,10 +13,13 @@ interface ListArrayObject {
 }
 
 function ToDoLists() {
-  let [list, setList] = useState<Array<ListArrayObject>>([]);
+  let retrieved : Array<ListArrayObject> = JSON.parse(localStorage.getItem("toDo") as string);
+  let [list, setList] = useState<Array<ListArrayObject>>(retrieved);
   let [activeFilter, setActiveFilter] = useState<string>("all");
   let [filteredArray,setFilterArray] = useState<Array<ListArrayObject>>([]);
-    
+  
+
+
   const addList = () => {
     let inputed : string = (document.getElementById("input") as HTMLInputElement).value;
     if(inputed === "") return;
@@ -29,13 +33,13 @@ function ToDoLists() {
     setList(list => [defaultObject,...list]);
   }
 
-  // useEffect(() => {
-  //  const currentActive = ((document.getElementById(activeFilter)) as HTMLInputElement);
-  //  currentActive.style.backgroundColor = "transparent";
-  //  currentActive.style.borderWidth = "2px";
-  //  currentActive.style.borderColor ="wheat";
-  //  currentActive.setAttribute("style","backgroundColor : transparent; border:2px solid black;");
-  // });
+  useEffect(() => {
+   const currentActive = ((document.getElementById(activeFilter)) as HTMLInputElement);
+   currentActive.style.backgroundColor = "transparent";
+   currentActive.style.borderWidth = "2px";
+   currentActive.style.borderColor ="wheat";
+   currentActive.setAttribute("style","backgroundColor : transparent; border:2px solid black;");
+  });
 
   useEffect(() => {
     let localStorageData : string = JSON.stringify(list);
@@ -54,9 +58,9 @@ function ToDoLists() {
     const setId: string = `${id}`;
     if(!isRemoved) return (
       <div className="listBox">
-       <div className="LBInput"> <input type="checkbox" onClick={() => changeCheckedState(setId)} id={setId} defaultChecked={isChecked}></input> </div>
-       <div className="LBHeading"> <h3> {listContent} </h3> </div>
-       <div className="LBButton"><button onClick={() => removeList(setId)}> <b>X</b> </button> </div>
+       <div className="LBInput"><input type="checkbox" onClick={() => changeCheckedState(setId)} id={setId} defaultChecked={isChecked}></input> </div>
+       <div className="LBHeading"><h3>{listContent}</h3></div>
+       <div className="LBButton"><button onClick={() => removeList(setId)}><b>X</b></button></div>
       </div>
     ) 
     return (
@@ -74,7 +78,6 @@ function ToDoLists() {
   }
 
   const removeList = (ofId : string) => {
-    // let tempArray : Array<ListArrayObject> = list;
     for(let element of list)
       if(element.id === parseInt(ofId)) 
         element.isRemoved = !element.isRemoved;
@@ -97,12 +100,18 @@ function ToDoLists() {
       </div>
       <div className="filterButtonsSection">  
         <div>
-          <button id = "all" onClick={() => changeActivefilter("all")}> All </button>
-          <button id = "completed" onClick={() => changeActivefilter("completed")}> Completed </button>
-          <button id = "active" onClick={() => changeActivefilter("active")}> Active </button>
+          <button style={changeBackground(activeFilter,"all")} id = "all" onClick={() => changeActivefilter("all")}> All </button>
+          <button style={changeBackground(activeFilter,"completed")} id = "completed" onClick={() => changeActivefilter("completed")}> Completed </button>
+          <button style={changeBackground(activeFilter,"active")} id = "active" onClick={() => changeActivefilter("active")}> Active </button>
         </div>
       </div>
     </div>
   );
 }
 export default ToDoLists;
+
+const changeBackground = (currentActive : string,toChangebackground : string) => {
+  if(currentActive === toChangebackground) return {
+    backgroundColor : 'red'
+  }
+}
